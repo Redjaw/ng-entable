f64e6659aff7b330d71f4d2af53c5d8c15efd5d1
Redjaw <davide.vernassa@gmail.com>
2016-11-24 23:00:37 +0100

    bower initialization and first structure

---
+++ ng-entable.js
@@ -0,0 +1,12 @@
+angular.module('ngEntable', [])
+.directive('ngEntable', function() {
+  return {
+  	scope : {
+
+  	},
+    templateUrl: 'partials/table.tpl.html',
+    link: function(scope,elem,attr){
+
+    }
+  };
+});
