c3f42f86d9e6339668a4f423138ac8ac2f32c4f8
Redjaw <davide.vernassa@gmail.com>
2016-12-15 23:59:02 +0100

    added provider

--- examples/app.js
+++ examples/app.js
@@ -2,25 +2,40 @@
 var myApp = angular.module('testApp', ['ngEntable']);
 
 myApp.controller('mainCtrl', function ($scope) {
-	$scope.tableModel = [{
-		"colA":"valA1",
-		"colB":"valB1",
-		"colC":"valC1"
-	},{
-		"colA":"valA2",
-		"colB":"valB2",
-		"colC":"valC2"
-	},{
-		"colA":"valA3",
-		"colB":"valB3",
-		"colC":"valC3"
-	}];
+	$scope.tableModel = {
+		
+		"labels":["ID","City","Population"],
+		"data":[{
+			"ID":"011",
+			"City":"turin",
+			"Population":"900000"
+		},{
+			"ID":"012",
+			"City":"milan",
+			"Population":"1200000"
+		},{
+			"ID":"013",
+			"City":"rome",
+			"Population":"2000000"
+		}],
+		"style":{
+			"table": {
+				"width":"100%"
+			},
+			"td" : {
+				"border-bottom":"1px solid grey"
+			},
+			"tr" : {
+				"height": "30px"
+			}
+		},
+		"options": {
+			"title":"City List"
 
-	$scope.tableStyle = {
-		"td" : {
-			"border":"1px solid grey"
 		}
-	}
+	};
 
-});
+}).config(["themeProvider", function (themeProvider) {
+        themeProvider.setTheme("test");
+    }]);
       
--- ng-entable.js
+++ ng-entable.js
@@ -3,14 +3,23 @@ angular.module('ngEntable', [])
   return {
   	restrict: "E",
   	scope : {
-  		enClass: "=?",
-  		enStyle: "=?",
   		enModel: "=?"
-
   	},
     templateUrl: '/partials/table.tpl.html',
+    controller: function($scope,theme){
+
+    },
     link: function(scope,elem,attr){
 
     }
+  };
+}).provider('theme', function() {
+
+  this.setTheme = function(themeNewName) {
+    this.themeName = themeNewName;
+  };
+
+  this.$get = function () {
+    return this;
   };
 });
